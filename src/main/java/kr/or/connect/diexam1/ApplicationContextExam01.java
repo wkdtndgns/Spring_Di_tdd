768695020b481f8776c04ce6e5202bd5800cd4b8
wkdtndgns <wkdtndgns@naver.com>
2018-07-11 18:54:18 +0900

    Di tdd

---
+++ src/main/java/kr/or/connect/diexam1/ApplicationContextExam01.java
@@ -0,0 +1,26 @@
+package kr.or.connect.diexam1;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class ApplicationContextExam01 {
+
+	public static void main(String[] args) {
+		ApplicationContext ac = new ClassPathXmlApplicationContext( 
+				"classpath:applicationContext.xml"); 
+		System.out.println("초기화 완료 !! ");
+		
+		UserBean userBean = (UserBean)ac.getBean("userBean");
+		userBean.setName("kim");
+		System.out.println(userBean.getName());
+		
+		UserBean userBean2 = (UserBean)ac.getBean("userBean");
+		
+		if(userBean == userBean2) {
+			System.out.println("같은 인스턴스입니다.");
+			
+		}
+		
+		}
+		
+	}
---
+++ src/main/java/kr/or/connect/diexam1/Car.java
@@ -0,0 +1,27 @@
+package kr.or.connect.diexam1;
+
+public class Car {
+	private Engine v8; 
+	
+	public Car() {
+		System.out.println("Car 생성자 ");
+	}
+	
+	public void setEngine(Engine e ) {
+		this.v8 =e;
+	}
+	
+	public void run() {
+		System.out.println("엔진을 이용하여 달립니다.");
+		v8.exec();
+	}
+	
+	public static void main(String[] args) {
+		Engine e = new Engine();
+		Car c = new Car();
+		c.setEngine(e);
+		c.run();
+	}
+	
+	
+}
